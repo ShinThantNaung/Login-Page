10f2afef8e254dde9a887425b1f181ad173ba337
ShinThantNaung <shinthantnaung112@gmail.com>
2025-09-23 17:41:16 +0630

    Fixed everything

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,22 +1,31 @@
 import "./App.css"
 import logo from "./assets/react.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGoogle,faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 
 function App(){
   return(
-    <div className = "container-fluid">
-      <div className = "form-container">
-        <div className = "logo-div"><img src = {logo}></img></div>
-        <div className="login-div">Login</div>
-        <div className="input-container">
-          <input className = "username-input" type = "text" placeholder = "Username" required />
-          <input className = "password-input" type = "password" placeholder = "Password" required />
-        </div>
-        <div className="link-container">
-          <p id = "register">Don't have an account? <a href = "#">Register!</a></p>
-        </div>
-        <div className="button-container">
-          <button type = "submit">Submit</button>
-        </div>
+    <div id = "container-fluid">
+      <div id="logo-div">
+        <img src = {logo} />
+      </div>
+      <div id="input-div">
+        <p id = "title">Login</p>
+        <input required class = "inputs" type = "email" placeholder = "Email"/>
+        <input required class = "inputs" type = "password" placeholder = "Password"/>
+      </div>
+      <div id="button-div">
+        <button type = "submit">Submit</button>
+        <p id = "forgot"><a href = "#">Forgot Password?</a></p>
+      </div>
+      <div id="socials-div">
+         <FontAwesomeIcon class = "socials" icon={faFacebook} />
+         <FontAwesomeIcon class = "socials" icon = {faGoogle} />
+         <FontAwesomeIcon class = "socials" icon = {faTwitter} />
+      </div>
+      <div id="register-div">
+        <p id = "register">Don't have an account?<a href = "#">Register!</a></p>
       </div>
     </div>
   )
